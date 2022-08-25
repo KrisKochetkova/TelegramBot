68f4d3d1f427e842e39806c0d2e8f6676bbd98e1
KrisKochetkova <110978358+KrisKochetkova@users.noreply.github.com>
2022-08-25 21:21:18 +0300

    Add files via upload

---
+++ table.js
@@ -0,0 +1,39 @@
+  let task = [
+    {
+      title: 'Часть 1. Структура кода',
+      link: 'https://jsmini.org/ru/statements/',
+      step: 0,
+    },
+    {
+      title: 'Часть 2. Переменные',
+      link: 'https://jsmini.org/ru/vars/',
+      step: 2,
+    },
+    {
+      title: 'Часть 3. Типы данных',
+      link: 'https://jsmini.org/ru/data-types/',
+      step: 2,
+    },
+    {
+      title: 'Часть 4. Преобразование типов',
+      link: 'https://jsmini.org/ru/type-conversions/',
+      step: 3,
+    },
+    {
+      title: 'Часть 5. Базовые операторы, математика',
+      link: 'https://jsmini.org/ru/operators/',
+      step: 2,
+    },
+    {
+      title: 'Часть 6. Операторы сравнения',
+      link: 'https://jsmini.org/ru/comparison-logical/',
+      step: 2,
+    },
+    {
+      title: 'Часть 7. Условное ветвление: if',
+      link: 'https://jsmini.org/ru/if-else/',
+      step: 3,
+    },
+  ];
+  
+  module.exports = {task};
